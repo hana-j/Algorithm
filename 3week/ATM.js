cd1eac701c4c8e8974fc792cb07eff498ae7d287
hana <aswjdgksk@gmail.con>
2022-01-08 21:31:30 +0900

    3w_2

---
+++ 3week/ATM.js
@@ -0,0 +1,18 @@
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+//const input = ['2','6 12 10','30 50 72'];
+
+//const A = input.shift();// 첫번째 값 제거
+//const n = input[0];
+const n = 5;
+const A = ['3 1 4 3 2'];
+
+const arr = A[0].split(' ').map(Number).sort((a,b)=>a-b);
+let arr1 =[];
+let total =0;
+let 더할값=0;
+for(let i =0; i<n; i++){
+    total +=더할값 +arr[i];
+    더할값 += arr[i];
+}
+console.log(total);
