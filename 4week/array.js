693107fb3a692481313633a364f9cab2d88d0ce2
hana <aswjdgksk@gmail.con>
2022-01-19 23:56:34 +0900

    5week

---
+++ 4week/array.js
@@ -0,0 +1,16 @@
+function array(rows, columns){
+    var arr = new Array(rows)
+    for(var i=0; i<rows; i++){
+        arr[i] = new Array(columns);
+    }
+    return arr;
+}
+var arr =array(0,0);
+arr.push([2,2])
+console.log(arr);
+
+class Solution{
+    function(m,n,arr){
+        
+    }
+}
